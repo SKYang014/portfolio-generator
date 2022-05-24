f52155d4516f0b9c95e5e3bcc6ce39683686f9b0
poofy <skyang014@gmail.com>
2022-05-23 21:33:29 -0700

    refactored filter and map

--- 9.3/app.js
+++ 9.3/app.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
+const mockData = {
+  name: 'Lernantino',
+  github: 'lernantino',
+  projects: []
+}
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -131,9 +137,9 @@ promptUser()
   .then(portfolioData => {
     console.log(portfolioData);
     // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(mockData);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
